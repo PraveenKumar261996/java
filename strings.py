6ae5059460504ad5670ba21e8c4f99e1735e5424
root <root@ip-172-31-86-164.ec2.internal>
2022-07-30 14:27:59 +0000

    Python File

---
+++ strings.py
@@ -0,0 +1,10 @@
+# single line comment
+letter = 'P'              #A String Could be a Single Character
+print(letter)             #P
+print(len(letter))        #1
+greeting = 'Hello, World!'#String Could be Single or Double
+print(greeting)           #Hello, World!
+print(len(greeting))      #13
+sentence = "I Hope You are Enjoying 30 days of Python Challenge"
+print(sentence)
+
